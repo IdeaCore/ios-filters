6fbce43506817889df8c2687bfdbfb9e349e2897
shirmung <shirmung@gmail.com>
2014-01-25 21:13:29 -0500

    initial xcode project

---
+++ ios-filters/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-filters
+//
+//  Created by Shirmung Bielefeld on 1/25/14.
+//  Copyright (c) 2014 Shirmung Bielefeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios-filters/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios-filters
+//
+//  Created by Shirmung Bielefeld on 1/25/14.
+//  Copyright (c) 2014 Shirmung Bielefeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
